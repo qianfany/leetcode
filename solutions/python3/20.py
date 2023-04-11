94622f5150fc680ccd51b35796ecdc68ade9c088
qianyang <qianfanyang@icloud.com>
2023-04-11 10:38:30 -0500

    4/11/2023 daily challenge

--- solutions/python3/20.py
+++ solutions/python3/20.py
@@ -6,4 +6,28 @@ class Solution:
                 brackets_stack.append(char)
             elif not brackets_stack or lefts.index(brackets_stack.pop()) != rights.index(char): 
                 return False
-        return not brackets_stack 
+        return not brackets_stack
+
+
+    def is_valid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+            elif not stack:
+                return False
+            elif char == ')' and stack.pop() != '(':
+                return False
+            elif char == ']' and stack.pop() != '[':
+                return False
+            elif char == '}' and stack.pop() != '{':
+                return False
+        return not stack
+
+
+if __name__ == '__main__':
+    zhushi = Solution()
+    s = "()[]{}"
+    print(zhushi.is_valid(s))
+
+
